3517db7f2dea5f5920214e6a5cfd0840373704a7
Aleksander <a.leksander@live.dk>
2023-04-25 10:46:36 +0200

    navigation, V1.1

--- Navigation/modultest/pathTest.cpp
+++ Navigation/modultest/pathTest.cpp
@@ -1,17 +1,22 @@
+#include "path.hpp" 
 #include <iostream>
-#include "path.hpp"
 
 int main() {
     std::vector<double> x_vals = {0.0, 1.0, 2.0, 3.0};
     std::vector<double> y_vals = {0.0, 1.0, 2.0, 3.0};
-    path my_path(x_vals, y_vals);
+    int bordNummer = 42;
+    path my_path(x_vals, y_vals, bordNummer);
 
     std::vector<double> x_copy = my_path.getXVals();
     std::vector<double> y_copy = my_path.getYVals();
 
+    std::cout << "Path points:" << std::endl;
     for (int i = 0; i < x_copy.size(); i++) {
         std::cout << "(" << x_copy[i] << ", " << y_copy[i] << ")" << std::endl;
     }
+
+    int bord_nummer = my_path.getBordNummer();
+    std::cout << "Board number: " << bord_nummer << std::endl;
 
     return 0;
 }
--- Navigation/path.hpp
+++ Navigation/path.hpp
@@ -2,13 +2,20 @@
 
 class path {
 public:
-    path(const std::vector<double>& x_vals, const std::vector<double>& y_vals)          
-        : x_vals_(x_vals), y_vals_(y_vals) {}
+    path(const std::vector<double>& x_vals, const std::vector<double>& y_vals, int bordNummer)          
+        : x_vals_(x_vals), y_vals_(y_vals), bordNummer_(bordNummer) {}
 
     std::vector<double> getXVals() const { return x_vals_; }
     std::vector<double> getYVals() const { return y_vals_; }
+    int getBordNummmer(){return bordNummer_}
+
+
+    //Mangler get FSK kode eller netværksid
 
 private:
     const std::vector<double> x_vals_;
     const std::vector<double> y_vals_;
+    const int bordNummer_;
+
+    //Mangler FSK kode eller netværksid
 };
