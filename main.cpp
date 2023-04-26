ef96ddfdf38b20db524ee86f889043c8f7ef7f5f
Aleksander <74602514+TheArduinoes@users.noreply.github.com>
2023-04-26 10:05:58 +0200

    MainV0.1
    Tilføjet en halvfærdig main.

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string>
+
+using namespace std;
+
+void error(string fejl)
+{
+
+}
+
+void *listen(void *arg) {
+    while (1) {
+        //lyt efter bord request, og tilføje til queue. 
+    }
+    return NULL;
+}
+
+int main()
+{
+    //init, hvor paths, lyttethread og pins sættes. 
+
+    //test væskehåndtering
+    if (/* condition */)
+    {
+        error("ERROR: Fejl ved væskehåndtering")
+    }
+    
+    while(running)
+    {
+
+        while(queue == 0)
+        {
+            //gå i standby mode, og checker queue
+        }
+
+        drivePath(queue(1));
+        driveHome(queue(1));
+        //slet den første i queue'en.
+        if(checkVaeske();)
+        {
+            //vent på bartender.
+        }
+        
+    }
+
+
+}
