10531d24be7aaaff092f68b09f3600b20d37dfa7
RodarRsol <100808870+RodarRsol@users.noreply.github.com>
2023-05-18 03:19:06 +0200

    test to Tackometer

---
+++ Tachometer/V1.2/Tachometer.cpp
@@ -0,0 +1,36 @@
+#include "Tachometer.h"
+
+Tachometer::Tachometer(int pin, double tire_circumference) :
+    m_pin(pin),
+    m_tire_circumference(tire_circumference),
+    m_previous_value(LOW),
+    m_distance(0.0),
+    m_drive(0)
+{
+    
+    digitalWrite(m_pin, LOW);
+    pinMode(m_pin, INPUT);
+    
+}
+
+void Tachometer::start(int drive)
+{
+    m_drive = drive;
+    m_distance=0.0;
+}
+
+double Tachometer::get_distance()
+{
+    int current_value = digitalRead(m_pin);
+    if (current_value == LOW && m_previous_value == HIGH) {
+        m_distance += m_tire_circumference;
+    }
+    m_previous_value = current_value;
+    return m_distance;
+}
+
+void Tachometer::reset_distance()
+{
+    m_distance = 0.0;
+}
+
---
+++ Tachometer/V1.2/Test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unistd.h>
+#include "Tachometer.h"
+
+using namespace std;
+
+
+int main()
+{
+    
+      wiringPiSetupGpio();
+    // Opret to instanser af Tachometer-klassen med de relevante pinnumre og dækomkredse
+    Tachometer rightTachometer(26, 2.5);
+    Tachometer leftTachometer(19, 2.5);
+
+
+     // Start måling for højre og venstre tachometre
+     double LEFT_DRIVE = 300.0;
+     double RIGHT_DRIVE = 300.0;
+    
+    rightTachometer.start(RIGHT_DRIVE);
+    leftTachometer.start(LEFT_DRIVE);
+
+   
+
+    while (true) {
+        double rightDistance = rightTachometer.get_distance();
+        double leftDistance = leftTachometer.get_distance();
+
+        cout << "RightDistance: " << rightDistance << endl;
+        cout << "LeftDistance: " << leftDistance << endl;
+        if (rightDistance >= RIGHT_DRIVE && leftDistance >= LEFT_DRIVE) { // Stop når 1000 cm er kørt
+         
+         // Nulstil afstandene
+         rightTachometer.reset_distance();
+         leftTachometer.reset_distance();
+
+            break;
+        }
+        delay(10); // Vent i 10 ms
+    }
+    
+    return 0;
+}
+
